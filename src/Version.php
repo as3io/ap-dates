a8e8f322b0656e5df1086baa08d63dfab71ddc8e
Jacob Bare <jacob.bare@cygnus.com>
2016-04-27 12:31:05 -0500

    Add version

---
+++ src/Version.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace As3\ApDates;
+
+/**
+ * AP dates version information.
+ *
+ * @author  Jacob Bare <jacob.bare@gmail.com>
+ */
+class Version
+{
+    const VERSION = '1.0.0';
+    const ID = 100000;
+    const MAJOR = 1;
+    const MINOR = 0;
+    const PATCH = 0;
+    const EXTRA = '';
+}
